4c36165db40f12930dc83ae3af70f61023c8a7f5
Kris_Chen <cw.akira@gmail.com>
2015-12-30 15:42:45 +0800

    add files

---
+++ AKGestureLock/AKGestureLock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AKGestureLock
+//
+//  Created by peoit-cw on 12/29/15.
+//  Copyright © 2015 SX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AKGestureLock/AKGestureLock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AKGestureLock
+//
+//  Created by peoit-cw on 12/29/15.
+//  Copyright © 2015 SX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
